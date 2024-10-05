2828be11953c15e5bb89e107fe887cc63350f4b8
Psalmantha <psalmanthaallaine03@gmail.com>
2024-10-05 23:18:03 +0800

    [FEAT] Add profile route with token authentication

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const { registerUser, loginUser } = require('../controllers/userController');
+const { registerUser, loginUser, getProfile } = require('../controllers/userController');
 const { validateUserRegistration } = require('../middleware/authMiddleware');
-const { getProfile } = require('../controllers/userController');
 const loginLimiter = require('../middleware/rateLimitMiddleware');
-const authMiddleware = require('../middleware/authMiddleware');
+const { verifyToken } = require('../utils/tokenUtils'); 
 
 const router = express.Router();
 
 router.post('/register', validateUserRegistration, registerUser);
+
 router.post('/login', loginLimiter, loginUser);
 
-router.get('/profile', authMiddleware, getProfile);
+router.get('/profile', verifyToken, getProfile);
 
 module.exports = router;
