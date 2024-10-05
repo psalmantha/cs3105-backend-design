b7dc6f18f6663dce87bfe7ce0b14c01a1ba19ab0
Psalmantha <psalmanthaallaine03@gmail.com>
2024-10-05 21:29:13 +0800

    [FEAT] Integrate rate limiter for login route for enhanced security

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { registerUser, loginUser } = require('../controllers/userController');
 const { validateUserRegistration } = require('../middleware/authMiddleware');
+const { loginLimiter } = require('../middleware/rateLimitMiddleware');
 
 const router = express.Router();
 
 router.post('/register', validateUserRegistration, registerUser);
-router.post('/login', loginUser);
+router.post('/login', loginLimiter, loginUser);
 
 module.exports = router;
