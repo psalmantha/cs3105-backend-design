64497579e4b2ebec44cc3439c30022a73b78f584
Psalmantha <psalmanthaallaine03@gmail.com>
2024-10-05 22:58:01 +0800

    [ADD] Controller logic for profile management

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const { createUser, verifyPassword } = require('../models/userModel');
+const { createUser, verifyPassword, findUserById } = require('../models/userModel');
 
 const registerUser = (req, res) => {
     const { username, email, password } = req.body;
@@ -22,4 +22,16 @@ const loginUser = (req, res) => {
     return res.status(200).json({ message: 'Login successful', token });
 };
 
-module.exports = { registerUser, loginUser };
+const getProfile = (req, res) => {
+    const userId = req.user.id;
+    
+    const user = findUserById(userId);
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+    }
+
+    const { id, username, email } = user;
+    return res.status(200).json({ id, username, email });
+};
+
+module.exports = { registerUser, loginUser, getProfile };
