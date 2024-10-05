68dc2f96d92dd6ed118ac724a0cc541a335f6be8
Psalmantha <psalmanthaallaine03@gmail.com>
2024-10-05 22:35:45 +0800

    [CHORE] Fix imports

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { registerUser, loginUser } = require('../controllers/userController');
 const { validateUserRegistration } = require('../middleware/authMiddleware');
-const { loginLimiter } = require('../middleware/rateLimitMiddleware');
+const loginLimiter = require('../middleware/rateLimitMiddleware');
 
 const router = express.Router();
 
