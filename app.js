e92ae9fe97b35cb4be3bf8cf0646adee80c60e19
Psalmantha <psalmanthaallaine03@gmail.com>
2024-10-05 22:20:17 +0800

    [CHORE] Remove global rateLimit middleware

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const userRoutes = require('./routes/user');
-const rateLimit = require('./middleware/rateLimitMiddleware');
 
 const app = express();
 app.use(express.json());
-app.use(rateLimit);
 
 app.use('/api', userRoutes);
 
@@ -13,7 +11,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('Internal server error');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
