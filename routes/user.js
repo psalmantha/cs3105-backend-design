5a873d662d86a00024af3a12ba1f2d2beb59907d
Psalmantha <psalmanthaallaine03@gmail.com>
2024-10-05 22:59:38 +0800

    [ADD] Create profile route

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const { registerUser, loginUser } = require('../controllers/userController');
 const { validateUserRegistration } = require('../middleware/authMiddleware');
+const { getProfile } = require('../controllers/userController');
 const loginLimiter = require('../middleware/rateLimitMiddleware');
+const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.post('/register', validateUserRegistration, registerUser);
 router.post('/login', loginLimiter, loginUser);
+
+router.get('/profile', authMiddleware, getProfile);
 
 module.exports = router;
