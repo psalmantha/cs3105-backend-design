79bbbfa0389ce19f0c83ca02349ec8578fe76b87
Psalmantha <psalmanthaallaine03@gmail.com>
2024-10-06 12:01:37 +0800

    [CHORE] Add dotenv configuration for env variable

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const userRoutes = require('./routes/user');
 
 const app = express();
+
 app.use(express.json());
 
 app.use('/api', userRoutes);
