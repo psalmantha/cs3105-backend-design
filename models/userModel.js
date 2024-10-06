ac2cfa462ad8f46fdea24e2a1aef9912e94abc3f
Psalmantha <psalmanthaallaine03@gmail.com>
2024-10-06 11:27:32 +0800

    [CHORE] Minor adjustments to user retrieval methods

--- models/userModel.js
+++ models/userModel.js
@@ -14,13 +14,18 @@ const findUser = (email) => {
     return users.find(user => user.email === email);
 };
 
+const findUserById = (id) => {
+    return users.find(user => user.id === id);
+};
+
 const verifyPassword = (email, password) => {
     const user = findUser(email);
-    return (user && user.password === password);
+    return user && user.password === password;
 };
 
 module.exports = {
     createUser,
     findUser,
-    verifyPassword
+    verifyPassword,
+    findUserById
 };
